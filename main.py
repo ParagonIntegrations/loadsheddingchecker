7121c37c58098674e6ea90cd4fee49b06a7cc845
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-08-11 11:10:58 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+class GetLoadSheddingStatus:
+
+    def __init__(self):
+        pass
+
+    def main(self):
+        print("It works")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    obj = GetLoadSheddingStatus()
+    obj.main()
+
+
